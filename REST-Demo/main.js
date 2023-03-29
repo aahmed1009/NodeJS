d1c16b07fdd4fae1bb5dff7561f4f398274df63b
Alaa Ahmed Gamal <61759061+aahmed1009@users.noreply.github.com>
2023-03-29 06:51:44 +0200

    CRUD

--- REST-Demo/main.js
+++ REST-Demo/main.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 let comments = [
   {
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
@@ -30,6 +39,46 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
 });
 
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+//TODO:CreateComment Route
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
+});
+
+//TODO:ShowDetails Route
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
+//TODO:Edit Route
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+//TODO:Update Route
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const foundComment = comments.find((c) => c.id === id);
+  const newCommentText = req.body.comment;
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
+});
+//TODO:DeleteComment Route
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
+});
+
 app.listen(3000, () => {
-  console.log("on port 3000!");
+  console.log("port 3000");
 });
